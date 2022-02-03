cbb91b82e024d668cfde16dbc5526e200dbe9b55
Radu-Bacila-CC <94835036+Radu-Bacila-CC@users.noreply.github.com>
2022-02-03 15:49:17 +0200

    Added SQL Database Connection

---
+++ MyShop.DataAccess.SQL/DataContext.cs
@@ -0,0 +1,30 @@
+﻿using MyShop.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.DataAccess.SQL
+{
+    public class DataContext : DbContext
+    {
+        public DataContext():base("DefaultConnection")
+        {
+            
+        }
+
+        //INTREBARE: Daca variabilele sunt publice, de ce folosim get, set?
+        //Nu ar trebui sa facem ca variabilele sa fie private si sa folosim metodele?
+
+        //INTREBARE: Comenzile Enable-Migrations / Add-Migration din Package Manager Console
+        //realizeaza conexiunea cu baza de date?
+        //Comenzile:
+        //Enable-Migration
+        //Add-Migration Initial
+        //Update-Database
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductCategory> ProductCategories { get; set; }
+    }
+}
