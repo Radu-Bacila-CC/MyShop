e41ec644dcdc2f4e7f596d42c40ceb365b47a03a
Radu-Bacila-CC <94835036+Radu-Bacila-CC@users.noreply.github.com>
2022-02-04 19:18:16 +0200

    Added customer model

--- MyShop.DataAccess.SQL/DataContext.cs
+++ MyShop.DataAccess.SQL/DataContext.cs
@@ -29,5 +29,6 @@ namespace MyShop.DataAccess.SQL
 
         public DbSet<Basket> Baskets { get; set; }
         public DbSet<BasketItem> BasketItems { get; set; }
+        public DbSet<Customer> Customers { get; set; }
     }
 }
