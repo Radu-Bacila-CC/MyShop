67f8f252ebfda00cbf01b1adaa604ddd2a332660
Radu-Bacila-CC <94835036+Radu-Bacila-CC@users.noreply.github.com>
2022-02-04 13:21:44 +0200

    Added Basket Model

---
+++ MyShop.Core/Models/Basket.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.Core.Models
+{
+    public class Basket : BaseEntity
+    {
+        
+
+        //INTREBARE: In tutorial declaratia este urmatoare:
+        //public virtual ICollection<BasketItem> BasketItems { get; set; }
+        //Dar primesc eroarea.
+        //Error CS0053  Inconsistent accessibility: property type 'ICollection<BasketItem>' is less accessible than
+        //property 'Basket.BasketItems'	
+        //Merge doar private
+
+        //Clasa BasketItem nu era public, am rezolvat
+        //Dar atunci acest tip de eroare vine de la faptul ca doua clase au accesibilitati diferite?
+
+        //public virtual ICollection<BasketItem> BasketItems { get; set; }
+        public virtual ICollection<BasketItem> BasketItems { get; set; }
+
+        public Basket()
+        {
+            this.BasketItems = new List<BasketItem>();
+        }
+    }
+}
--- MyShop.DataAccess.SQL/DataContext.cs
+++ MyShop.DataAccess.SQL/DataContext.cs
@@ -26,5 +26,8 @@ namespace MyShop.DataAccess.SQL
         //Update-Database
         public DbSet<Product> Products { get; set; }
         public DbSet<ProductCategory> ProductCategories { get; set; }
+
+        public DbSet<Basket> Baskets { get; set; }
+        public DbSet<BasketItem> BasketItems { get; set; }
     }
 }
